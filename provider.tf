7fbb48b32cabe02c582899226406f795f9c6194a
lmgaspa <@luhmgasparetto@gmaail.com>
2024-10-23 16:52:33 -0300

    refactor

--- provider.tf
+++ provider.tf
@@ -2,21 +2,41 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
+      version = "4.48.0"
     }
-  }
-
-  backend "s3" {
-    bucket = "comunidadedevopsluiz"
-    key    = "dev/terraform.tfstate"
-    region = "us-east-1"
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.17.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.8.0"
+    }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
 
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+provider "kubernetes" {
+  host                   = module.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+    command     = "aws"
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks_cluster.endpoint
+    cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+      command     = "aws"
+    }
+  }
 }
